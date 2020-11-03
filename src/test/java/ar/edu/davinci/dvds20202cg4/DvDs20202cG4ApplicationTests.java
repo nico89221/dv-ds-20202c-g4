6220c1a8d4d6f6137494355b66ba4257ff062163
nico89221 <nico89221@hotmail.com>
2020-11-02 21:10:39 -0300

    Se cambio el git

---
+++ src/test/java/ar/edu/davinci/dvds20202cg4/DvDs20202cG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20202cG4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

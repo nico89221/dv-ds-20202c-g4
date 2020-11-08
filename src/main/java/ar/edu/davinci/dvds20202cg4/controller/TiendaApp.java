f71ad4832f2b7557177de500a2124b865c5b1a09
nico89221 <nico89221@hotmail.com>
2020-11-08 17:25:04 -0300

    Se crearon los siguiente paquetes:
    controller-->view
    		  -->rest-->con la clase TiendaApp

--- src/main/java/ar/edu/davinci/dvds20202cg4/controller/TiendaApp.java
+++ src/main/java/ar/edu/davinci/dvds20202cg4/controller/TiendaApp.java
@@ -1,5 +1,12 @@
 package ar.edu.davinci.dvds20202cg4.controller;
 
-public class TiendaApp {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping(path="/tienda")
+public abstract class TiendaApp {
+
+    
+    // http://localhost:8091/tienda
 }

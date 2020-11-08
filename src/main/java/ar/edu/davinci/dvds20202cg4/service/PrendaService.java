ad17ae9c59966f1e9f1477b24750ce834d1eeb70
nico89221 <nico89221@hotmail.com>
2020-11-08 15:57:57 -0300

    Se actualizaron las clases PrendaService y PrendaServiceImpl.

---
+++ src/main/java/ar/edu/davinci/dvds20202cg4/service/PrendaService.java
@@ -0,0 +1,22 @@
+package ar.edu.davinci.dvds20202cg4.service;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import ar.edu.davinci.dvds20202cg4.model.Prenda;
+
+@Service
+public interface PrendaService {
+
+	public List<Prenda> listAll();
+	public Page<Prenda> list(Pageable pageable);
+	public Prenda findById(Long id);
+	public Prenda save(Prenda prenda);
+	public void delete(Prenda prenda);
+	Page<Prenda> List(org.springframework.data.domain.Pageable pageable);
+	
+	
+}
---
+++ src/main/java/ar/edu/davinci/dvds20202cg4/service/PrendaServiceImpl.java
@@ -0,0 +1,70 @@
+package ar.edu.davinci.dvds20202cg4.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import ar.edu.davinci.dvds20202cg4.model.Prenda;
+import ar.edu.davinci.dvds20202cg4.repository.PrendaRepository;
+
+
+public class PrendaServiceImpl implements PrendaService {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(PrendaServiceImpl.class);
+
+	
+	private final PrendaRepository prendaRepository;
+	
+	
+	@Autowired
+    public PrendaServiceImpl(final PrendaRepository prendaRepository) {
+        this.prendaRepository = prendaRepository;
+    }
+
+    @Override
+    public List<Prenda> listAll() {
+        return prendaRepository.findAll();
+    }
+
+    @Override
+    public Page<Prenda> List(Pageable pageable) {
+        LOGGER.info("Pagegable: offset: " + pageable.getOffset() + " - pageSize:" + pageable.getPageSize());
+        return prendaRepository.findAll(pageable);
+    }
+
+    @Override
+    public Prenda findById(Long id) {
+        Optional<Prenda> prendaOptional = prendaRepository.findById(id);
+        if (prendaOptional.isPresent()) {
+            return prendaOptional.get();
+        }
+        return null;
+    }
+
+    @Override
+    public Prenda save(Prenda prenda) {
+        // TODO Auto-generated method stub
+        return prendaRepository.save(prenda);
+    }
+
+
+	@Override
+    public void delete(Prenda prenda) {
+        prendaRepository.delete(prenda);
+
+	}
+
+
+	@Override
+	public Page<Prenda> list(java.awt.print.Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

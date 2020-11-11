9b22bb51e021a52a9f4cc92b9bf05e4c65dff63d
nico89221 <nico89221@hotmail.com>
2020-11-11 17:44:22 -0300

    Se creo la clase OrikaConfiguration y se modifico el pom.xml. Tambien se crearon las siguientes clases: PrendaInsertRequest PrendaUpdateRequest PrendaResponse

---
+++ src/main/java/ar/edu/davinci/dvds20202cg4/controller/rest/request/PrendaUpdateRequest.java
@@ -0,0 +1,23 @@
+package ar.edu.davinci.dvds20202cg4.controller.rest.request;
+
+import java.math.BigDecimal;
+
+import ar.edu.davinci.dvds20202cg4.model.TipoPrenda;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PrendaUpdateRequest {
+    
+    private BigDecimal precioBase;
+    
+    private TipoPrenda tipo;
+    
+    private String descripcion;
+
+}
